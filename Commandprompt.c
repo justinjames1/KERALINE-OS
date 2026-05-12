4499775bffac4f3ab9e78767e4ad184d8cf4bf34
justindude057-collab <justindude057@gmail.com>
2026-05-11 21:49:07 -0400

    Update Commandprompt.c

--- Commandprompt.c
+++ Commandprompt.c
@@ -4,29 +4,29 @@ int main(){
 int T = 1;
 int i = 40096;
 char name[50];
-scanf(”%s”, name);
-printf(“C:/user/kernel/= %s”,name);
-printf(”\n”);
+scanf("%s", name);
+printf("C:/user/kernel/= %s",name);
+printf("\n");
 int currentprocs = 0;
 int processid = 0;
 void createprocess(){
 processid ++;
-printf(“process created.\n”);
-currentprocs ++ ;
+printf("process created.\n");
+currentprocs ++;
 int mem[i];}
 void tasks(){
-printf(“current processes are. “);
-printf(”%d\n”,currentprocs);}
+printf("current processes are. ");
+printf("%d\n",currentprocs);}
 void panic(){
-printf(”=============\n”);
-printf(“KERNEL PANIC.\n”);
-printf(”=============\n”);
-printf(“ERROR 0X000000001\n”);}
+printf("=============\n");
+printf("KERNEL PANIC.\n");
+printf("=============\n");
+printf("ERROR 0X000000001\n");}
 void alloc(){
 int mem[i];
-printf(“allocated %d”,i);}
+printf("allocated %d",i);}
 while (T == 1){
-if (strcmp(name, “panic”) == 0){panic();}
-if (strcmp(name, “createprocess”) == 0){createprocess();}
-if (strcmp(name, “alloc”) == 0){alloc();}
+if (strcmp(name, "panic") == 0){panic();}
+if (strcmp(name, "createprocess") == 0){createprocess();}
+if (strcmp(name, "alloc") == 0){alloc();}
 break;}}
